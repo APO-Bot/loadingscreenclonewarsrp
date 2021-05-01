0febd739d776b3e5d7b9f330c08b784c650b0650
APO <59201044+APO-Bot@users.noreply.github.com>
2021-05-01 21:08:50 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "0DD1E7CA5D1A776CCEF02F36845D89FB"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="Clone Wars RP"; // For your Browser Name
+$community_name ="[Village Community] Clone Wars RP | L'Eres des Clones"; // Your community/server name. The one in the middle
+$community_url = "https://village-commu.atlina.fr/index.php"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " Il y a longtemps dans un serveur,<br> lointain...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Bienvenue soldat!"; // Above your server name
+// Under Server Name
+$sub_title_2 = "Nous t'attendons au front!"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "Qui sommes-nous ?";
+$left_bottom_sub_title = "https://discord.gg/7yVTMPEeTU";
+$right_bottom_title = "";
+$right_bottom_sub_title = "";
+
+$left_bottom_text = "";
+
+?>
